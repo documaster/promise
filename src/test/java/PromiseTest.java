86773f59d823d06e5cc7948356eee90d3f468e93
Martin Nestorov <mnestorov@protonmail.com>
2020-10-04 15:28:26 +0300

    Add basic tests and until/schedule tests

--- src/test/java/PromiseTest.java
+++ src/test/java/PromiseTest.java
@@ -15,19 +15,26 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 import main.Promise;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PromiseTest {
 
 	@Test
-	public void testPromiseRetryCancel() throws Exception {
+	public void promiseRetryCancel() throws Exception {
 
 		final int[] numFails = { 0 };
 		final int sleepMs = 500;
@@ -55,7 +62,7 @@ public class PromiseTest {
 	}
 
 	@Test
-	public void testPromiseRetry() throws Exception {
+	public void promiseRetry() throws Exception {
 
 		final int[] numFails = { 2 };
 
@@ -86,4 +93,115 @@ public class PromiseTest {
 		Promise.retry(supplier, numFails[0] + 1).join();
 	}
 
+	@Test
+	public void none() throws Exception {
+
+		assertNull(Promise.none().join());
+	}
+
+	@Test
+	public void empty() throws Exception {
+
+		assertEquals(Promise.empty().join(), Optional.empty());
+	}
+
+	@Test
+	public void completed() throws Exception {
+
+		assertEquals(Promise.completed(100).join(), 100);
+		Supplier<Integer> s = () -> { return 1000; };
+		assertEquals(Promise.completed(s.get()).join(), 1000);
+		assertNull(Promise.completed(Promise.none().join()).join());
+	}
+
+	@Test
+	public void fail() throws Exception {
+
+		Throwable ex = new Throwable("Error");
+		CompletableFuture<Object> fail = Promise.fail(ex);
+
+		assertNotNull(fail);
+		assertTrue(fail.isCompletedExceptionally());
+	}
+
+	@Test
+	public void runUntil() throws Exception {
+
+		final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
+
+		Supplier<Integer> supplier = () -> { return 1000; };
+
+		// Set the time units to a small number so that the tests pass quickly
+		Promise.runUntil(supplier, 1000, TimeUnit.MILLISECONDS, scheduledExecutor);
+
+		assertFalse(scheduledExecutor.isShutdown());
+
+		Thread.sleep(1000);
+
+		assertTrue(scheduledExecutor.isShutdown());
+	}
+
+	@Test
+	public void runAtMostUntil() throws Exception {
+
+		final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
+
+		Supplier<Integer> supplier = () -> { return 1000; };
+
+		// Set the time units to a small number so that the tests pass quickly
+		Promise.runAtMostUntil(supplier, 1000, 1000, TimeUnit.MILLISECONDS, scheduledExecutor);
+		assertFalse(scheduledExecutor.isShutdown());
+		Thread.sleep(1000);
+		assertTrue(scheduledExecutor.isShutdown());
+
+		Promise.runAtMostUntil(supplier, 1000, 100, TimeUnit.MILLISECONDS, scheduledExecutor);
+		assertTrue(scheduledExecutor.isShutdown());
+
+		Promise.runAtMostUntil(supplier, 1000, 10000, TimeUnit.MILLISECONDS, scheduledExecutor);
+		assertTrue(scheduledExecutor.isShutdown());
+
+		Promise.runAtMostUntil(supplier, 100, 1000, TimeUnit.MILLISECONDS, scheduledExecutor);
+		assertTrue(scheduledExecutor.isShutdown());
+
+		Promise.runAtMostUntil(supplier, 10000, 1000, TimeUnit.MILLISECONDS, scheduledExecutor);
+		assertTrue(scheduledExecutor.isShutdown());
+	}
+
+	@Test
+	public void delay() throws Exception {
+
+		final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
+
+		Supplier<Integer> supplier = () -> { return 0; };
+
+		// Set the time units to a small number so that the tests pass quickly
+		CompletableFuture<Void> delay = Promise.delay(1000, TimeUnit.MILLISECONDS, scheduledExecutor);
+		delay.thenRun(() -> supplier.get());
+
+		for (int i = 0; i < 1000; i++) {
+			Thread.sleep(1);
+		}
+
+		assertTrue(delay.isDone());
+	}
+
+	@Test
+	public void delaySupplier() throws Exception {
+
+		final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
+
+		Supplier<Integer> supplier = () -> { return 1000; };
+
+		// Set the time units to a small number so that the tests pass quickly
+		CompletableFuture<Integer> delay = Promise.delay(supplier, 1000, TimeUnit.MILLISECONDS, scheduledExecutor);
+
+		int delayCounter = 0;
+		for (int i = 0; i < 1000; i++) {
+			Thread.sleep(1);
+			delayCounter += 1;
+		}
+
+		assertTrue(delay.isDone());
+		assertEquals(delay.get(), delayCounter);
+	}
 }
